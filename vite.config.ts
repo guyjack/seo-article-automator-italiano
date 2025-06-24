2e9e18ae814fbdb8f68368fc3774c6e21183f7e5
guyjack <giuseppe_campomilla@yahoo.it>
2025-06-24 22:53:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+// Aggiungi in cima al file vite.config.js o main.js
+import { webcrypto as crypto } from 'crypto';
+if (!globalThis.crypto) globalThis.crypto = crypto;
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
